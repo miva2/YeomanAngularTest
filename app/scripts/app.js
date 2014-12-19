567248f7da7ca4b9bd466d4cc1be68a6d53c4cdd
Michael Vandendriessche <michael.vandendriessche@ordina.be>
2014-12-19 13:20:39 +0100

    Yeoman tutorial completed, step 5 - 10: used code from their github. Kept own code in comment in app.js, main.js and main.html

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,33 +1,62 @@
+// 'use strict';
+//
+// /**
+//  * @ngdoc overview
+//  * @name lorfrontendApp
+//  * @description
+//  * # lorfrontendApp
+//  *
+//  * Main module of the application.
+//  */
+// angular
+//   .module('lorfrontendApp', [
+//     'ngAnimate',
+//     'ngCookies',
+//     'ngResource',
+//     'ngRoute',
+//     'ngSanitize',
+//     'ngTouch',
+//     'ui.sortable'
+//     'LocalStorageModule'
+//   ])
+//   .config(['localStorageServiceProvider', function(localStorageServiceProvider){
+//     localStorageServiceProvider.setPrefix('ls');
+//   }])
+//   .config(function ($routeProvider) {
+//     $routeProvider
+//       .when('/', {
+//         templateUrl: 'views/main.html',
+//         controller: 'MainCtrl'
+//       })
+//       .when('/about', {
+//         templateUrl: 'views/about.html',
+//         controller: 'AboutCtrl'
+//       })
+//       .otherwise({
+//         redirectTo: '/'
+//       });
+//   });
+
+
+//////Code  from github example
 'use strict';
 
-/**
- * @ngdoc overview
- * @name lorfrontendApp
- * @description
- * # lorfrontendApp
- *
- * Main module of the application.
- */
-angular
-  .module('lorfrontendApp', [
-    'ngAnimate',
-    'ngCookies',
-    'ngResource',
-    'ngRoute',
-    'ngSanitize',
-    'ngTouch'
-  ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl'
-      })
-      .when('/about', {
-        templateUrl: 'views/about.html',
-        controller: 'AboutCtrl'
-      })
-      .otherwise({
-        redirectTo: '/'
-      });
+angular.module('mytodoApp', [
+'ngCookies',
+'ngResource',
+'ngSanitize',
+'ngRoute',
+'ui.sortable',
+'LocalStorageModule'
+])
+.config(['localStorageServiceProvider', '$routeProvider', function(localStorageServiceProvider, $routeProvider){
+  localStorageServiceProvider.setPrefix('ls');
+  $routeProvider
+  .when('/', {
+    templateUrl: 'views/main.html',
+    controller: 'MainCtrl'
+  })
+  .otherwise({
+    redirectTo: '/'
   });
+}]);
--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -1,17 +1,60 @@
+// 'use strict';
+//
+// /**
+//  * @ngdoc function
+//  * @name lorfrontendApp.controller:MainCtrl
+//  * @description
+//  * # MainCtrl
+//  * Controller of the lorfrontendApp
+//  */
+// angular.module('lorfrontendApp')
+//   .controller('MainCtrl', function ($scope, localStorageService) {
+//
+//     var todosInStore = localStorageService.get('todos');
+//
+//     $scope.todos = todosInStore || [];
+//     // $scope.todos = [item1, item2, item3];
+//
+//     $scope.$watch('todos', function() {
+//       localStorageService.add('todos', $scope.todos);
+//     }, true);
+//
+//     $scope.addTodo = function () {
+//       $scope.todos.push($scope.todo);
+//       $scope.todo = '';
+//     };
+//
+//     $scope.removeTodo = function(index) {
+//       $scope.todos.splice(index, 1);
+//     };
+//   });
+
+
+//////Code from github example
+
 'use strict';
 
-/**
- * @ngdoc function
- * @name lorfrontendApp.controller:MainCtrl
- * @description
- * # MainCtrl
- * Controller of the lorfrontendApp
- */
-angular.module('lorfrontendApp')
-  .controller('MainCtrl', function ($scope) {
-    $scope.awesomeThings = [
-      'HTML5 Boilerplate',
-      'AngularJS',
-      'Karma'
-    ];
-  });
+angular.module('mytodoApp')
+.controller('MainCtrl', function ($scope, localStorageService) {
+
+  var todosInStore = localStorageService.get('todos');
+
+  $scope.todos = todosInStore || [];
+
+  $scope.$watch('todos', function () {
+    localStorageService.add('todos', $scope.todos);
+  }, true);
+
+  // Uncomment if you are disabling persistence
+  //$scope.todos = [];
+
+  $scope.addTodo = function () {
+    $scope.todos.push($scope.todo);
+    $scope.todo = '';
+  };
+
+  $scope.removeTodo = function (index) {
+    $scope.todos.splice(index, 1);
+  };
+
+});
